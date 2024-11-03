e6da47e57d42bcdc30fd95f2dae105df1489e17a
Ariel Del Valle Lozano <arielmazatlan@gmail.com>
2024-11-03 03:14:54 +0000

    Replaced Storage with File

--- tests/Unit/ValidationAssertionsPhpdocTest.php
+++ tests/Unit/ValidationAssertionsPhpdocTest.php
@@ -4,7 +4,7 @@ namespace Arielenter\ValidationAssertions\Tests\Unit;
 
 use Arielenter\ValidationAssertions\Tests\Support\TransAssertions;
 use Arielenter\ValidationAssertions\Tests\TestCase;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 use PHPUnit\Framework\Attributes\Test;
 
 class ValidationAssertionsPhpdocTest extends TestCase {
@@ -13,9 +13,8 @@ class ValidationAssertionsPhpdocTest extends TestCase {
 
     #[Test]
     public function assertValidationRulesAreImplementedInUrl_phpdoc(): void {
-        $storage = Storage::createLocalDriver(['root' => __DIR__ . '/../../']);
         $filePath = 'src/Assertions.php';
-        $oldCode = $storage->get($filePath);
+        $oldCode = File::get($filePath);
 
         $start = preg_quote('/**', '/');
         $end = 'assertValidationRulesAreImplementedInUrl';
@@ -28,10 +27,9 @@ class ValidationAssertionsPhpdocTest extends TestCase {
 
         if ($oldPhpdoc != $newPhpdoc) {
             $newCode = str_replace($oldPhpdoc, $newPhpdoc, $oldCode);
-            $storage->put($filePath, $newCode);
+            File::put($filePath, $newCode);
         }
 
-        $this->assertStringContainsString($newPhpdoc, $storage->get(
-                        $filePath));
+        $this->assertStringContainsString($newPhpdoc, File::get($filePath));
     }
 }
